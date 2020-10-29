5a10388007fbbf96bd6192e341ae1dfe9f7600a3
Steven Toth <steven.toth@ltnglobal.com>
2020-10-28 20:53:36 -0400

    streammodel: bugfixes and defensive code to restart the model under extreme stream change conditions

--- src/streammodel.c
+++ src/streammodel.c
@@ -18,7 +18,11 @@
 #include "libltntstools/ts.h"
 #include "libltntstools/pat.h"
 
-static void message(dvbpsi_t *handle, const dvbpsi_msg_level_t level, const char* msg);
+#if 0
+#define DVBPSI_REPORTING (DVBPSI_MSG_DEBUG)
+#else
+#define DVBPSI_REPORTING (DVBPSI_MSG_ERROR)
+#endif
 
 /* Running Object Model: A model of an entire ISO13818 stream,
  * Including PAT/PMT configurations, PIDS being used, when and how.
@@ -63,6 +67,10 @@ struct streammodel_rom_s
 	int modelComplete;	/* Boolean. */
 
 	struct timeval allowableWriteTime;
+	struct timeval pmtCollectionTimer; /* We have until this time expires to collect all PMTs, else
+					    * We have a mis-configured stream, or the stream changed behind the frameworks back
+					    * during PMT collection, and the new PMT never arrived.
+					    */
 
 #define MAX_ROM_PIDS 0x2000
 	struct streammodel_pid_s pids[MAX_ROM_PIDS];
@@ -78,6 +86,7 @@ struct streammodel_ctx_s
 	 * 'current' points to the last known good model. 
 	 * into 'current' when its considered complete and its safe to switch pointers.
 	 */
+	uint64_t currentModelVersion;
 	pthread_mutex_t rom_mutex;
 	struct streammodel_rom_s *current;	/* Model any user queries will run against. */
 	struct streammodel_rom_s *next;		/* Model currently being build by the framework, never user accessible. */
@@ -85,7 +94,14 @@ struct streammodel_ctx_s
 
 	/* Housekeeping */
 	struct timeval now;			/* Each write() call updates this */
+
+	/* */
+	int writePackets;
+	int restartModel;
 };
+
+static void message(dvbpsi_t *handle, const dvbpsi_msg_level_t level, const char* msg);
+static int _streammodel_query_model(struct streammodel_ctx_s *ctx, struct streammodel_rom_s *rom, struct ltntstools_pat_s **pat);
 
 /* ROM */
 static void _rom_next_complete(struct streammodel_ctx_s *ctx)
@@ -106,18 +122,20 @@ static void _rom_initialize(struct streammodel_ctx_s *ctx, struct streammodel_ro
 		if (ps->p_pat) {
 			dvbpsi_pat_delete(ps->p_pat);
 			ps->p_pat = NULL;
-			dvbpsi_pat_detach(ps->p_dvbpsi);
-			ps->p_dvbpsi = NULL;
 		}
+		if (ps->pidType == PT_PAT && ps->p_dvbpsi) {
+			dvbpsi_pat_detach(ps->p_dvbpsi);
+		}
+
 		if (ps->p_pmt) {
 			dvbpsi_pmt_delete(ps->p_pmt);
 			ps->p_pmt = NULL;
+		}
+		if (ps->pidType == PT_PMT && ps->p_dvbpsi) {
 			dvbpsi_pmt_detach(ps->p_dvbpsi);
-			ps->p_dvbpsi = NULL;
 		}
 
 		if (ps->p_dvbpsi) {
-printf("pid 0x%x\n", i);
 			dvbpsi_delete(ps->p_dvbpsi);
 			ps->p_dvbpsi = NULL;
 		}
@@ -130,10 +148,43 @@ printf("pid 0x%x\n", i);
 	rom->modelComplete = 0;
 	rom->parsedPMTs = 0;
 	rom->totalPMTsInPAT = 0;
+	rom->pmtCollectionTimer.tv_sec = 0;
 }
 
 void _rom_activate(struct streammodel_ctx_s *ctx)
 {
+	/* Compare current and next models, bounce the version if
+	 * we've detected a change.
+	 */
+
+	int ret;
+
+	struct ltntstools_pat_s *patCurrent = NULL;
+	struct ltntstools_pat_s *patNext = NULL;
+	ret = _streammodel_query_model(ctx, ctx->current, &patCurrent);
+	ret = _streammodel_query_model(ctx, ctx->next, &patNext);
+
+	if (patCurrent && patNext) {
+		ret = ltntstools_pat_compare(patCurrent, patNext);
+		if (ret != 0) {
+			ctx->currentModelVersion++;
+			printf("*** NEW MODEL DETECTED as 0x%016" PRIx64 "***\n", ctx->currentModelVersion);
+		} else {
+#if 0
+			printf("*** current/next models are identical, no changes detected ***\n");
+#endif
+		}
+	}
+
+	if (patCurrent) {
+		ltntstools_pat_free(patCurrent);
+		patCurrent = NULL;
+	}
+	if (patNext) {
+		ltntstools_pat_free(patNext);
+		patNext = NULL;
+	}
+
 	/* Promote the 'next' rom */
 	struct streammodel_rom_s *c = ctx->current;
 
@@ -149,14 +200,19 @@ void _rom_activate(struct streammodel_ctx_s *ctx)
 	timeradd(&future, &ctx->now, &ctx->next->allowableWriteTime);
 }
 
+static struct streammodel_pid_s *_rom_find_pid(struct streammodel_rom_s *rom, uint16_t pid)
+{
+	return &rom->pids[pid];
+}
+
 static struct streammodel_pid_s *_rom_current_find_pid(struct streammodel_ctx_s *ctx, uint16_t pid)
 {
-	return &ctx->current->pids[pid];
+	return _rom_find_pid(ctx->current, pid);
 }
 
 static struct streammodel_pid_s *_rom_next_find_pid(struct streammodel_ctx_s *ctx, uint16_t pid)
 {
-	return &ctx->next->pids[pid];
+	return _rom_find_pid(ctx->next, pid);
 }
 
 /* End: ROM */
@@ -174,6 +230,8 @@ static void message(dvbpsi_t *handle, const dvbpsi_msg_level_t level, const char
 	fprintf(stderr, "%s\n", msg);
 }
 
+#define CHATTY_CALLBACKS 0
+
 static void cb_pmt(void *p_zero, dvbpsi_pmt_t *p_pmt)
 {
 	struct streammodel_pid_s *ps = p_zero;
@@ -182,12 +240,14 @@ static void cb_pmt(void *p_zero, dvbpsi_pmt_t *p_pmt)
 
 	dvbpsi_pmt_es_t* p_es = p_pmt->p_first_es;
 
+#if CHATTY_CALLBACKS
 	printf("New active PMT (%d)\n", ps->pid);
 	printf("  program_number : %d\n", p_pmt->i_program_number);
-
+#endif
 	while(p_es) {
-		printf("    pid %04x estype %02x\n", p_es->i_pid, p_es->i_type);
-
+#if CHATTY_CALLBACKS
+		printf("    pid 0x%04x estype %02x\n", p_es->i_pid, p_es->i_type);
+#endif
 		struct streammodel_pid_s *es = _rom_next_find_pid(ctx, p_es->i_pid);
 		es->present = 1;
 		es->pidType = PT_ES;
@@ -200,7 +260,9 @@ static void cb_pmt(void *p_zero, dvbpsi_pmt_t *p_pmt)
 
 	rom->parsedPMTs++;
 
+#if CHATTY_CALLBACKS
 	printf("%s() parsed %d expecting %d\n", __func__, rom->parsedPMTs, rom->totalPMTsInPAT);
+#endif
 	if (rom->parsedPMTs == rom->totalPMTsInPAT) {
 		printf("Model#%d collection complete, %d PMTs collected\n", rom->nr, rom->parsedPMTs);
 		_rom_next_complete(ctx);
@@ -218,16 +280,43 @@ static void cb_pat(void *p_zero, dvbpsi_pat_t *p_pat)
 
 	dvbpsi_pat_program_t *p_program = p_pat->p_first_program;
 
+	if (rom->totalPMTsInPAT) {
+		/* We might already have a PAT in progress. If we do, and another pat has
+		 * arrived with a differnt version number, that means the stream was
+		 * randomly changed underneat is, and we're not receiving
+		 * a completely different stream.
+		 * Abort the processing of the previous PAT, cleanup the rom and start again
+		 * with the PAT.
+		 */
+		struct streammodel_pid_s *m = _rom_next_find_pid(ctx, 0);
+		if (m->p_pat->i_version != p_pat->i_version && p_pat->b_current_next) {
+			printf("New PAT arrived before the prior PAT complete version 0x%02x vs 0x%02x.\n",
+				m->p_pat->i_version, p_pat->i_version);
+			dvbpsi_pat_delete(p_pat);
+			ctx->restartModel = 1;
+			return;
+		}
+	}
+
+#if CHATTY_CALLBACKS
 	printf("\n");
 	printf("PAT\n");
 	printf("  transport_stream_id : 0x%04x\n", p_pat->i_ts_id);
 	printf("  version_number      : 0x%04x\n", p_pat->i_version);
+	printf("  current_next        : %d\n", p_pat->b_current_next);
 	printf("    | program_number @ PID\n");
+#endif
+
+	/* Maximum of 1 second to gather PMT. */
+	struct timeval future = { 1, 0 };
+	timeradd(&future, &ctx->now, &ctx->next->pmtCollectionTimer);
 
 	rom->totalPMTsInPAT = 0;
 
 	while(p_program) {
+#if CHATTY_CALLBACKS
 		printf("    | %14d @ 0x%04x (%d)\n", p_program->i_number, p_program->i_pid, p_program->i_pid);
+#endif
 
 		/* Program# 0 is reserved for NIT tables. We don't expect a PMT for these. */
 		if (p_program->i_number > 0) {
@@ -237,7 +326,13 @@ static void cb_pat(void *p_zero, dvbpsi_pat_t *p_pat)
 			m->present = 1;
 			m->pidType = PT_PMT;
 
-			m->p_dvbpsi = dvbpsi_new(&message, DVBPSI_MSG_DEBUG);
+			if (m->p_dvbpsi) {
+				dvbpsi_pmt_detach(m->p_dvbpsi);
+				dvbpsi_delete(m->p_dvbpsi);
+				m->p_dvbpsi = NULL;
+			}
+
+			m->p_dvbpsi = dvbpsi_new(&message, DVBPSI_REPORTING);
 			if (!m->p_dvbpsi) {
 				fprintf(stderr, "%s() PSI alloc. Should never happen\n", __func__);
 				exit(1);
@@ -255,8 +350,10 @@ static void cb_pat(void *p_zero, dvbpsi_pat_t *p_pat)
 		p_program = p_program->p_next;
 
 	}
+#if CHATTY_CALLBACKS
 	printf(  "  active              : %d\n", p_pat->b_current_next);
 	printf(  "  PMTS %d\n", rom->totalPMTsInPAT);
+#endif
 
 	/* Don't delete p_pat, we're caching it. */
 	ps->p_pat = p_pat;
@@ -296,13 +393,28 @@ size_t ltntstools_streammodel_write(void *hdl, const unsigned char *pkt, int pac
 
 	gettimeofday(&ctx->now, NULL);
 
-	int writePackets = 0;
+	ctx->writePackets = 0;
 
 	if (timercmp(&ctx->now, &ctx->next->allowableWriteTime, >=)) {
-		writePackets = 1;
+		ctx->writePackets = 1;
+	}
+	if (ctx->next->pmtCollectionTimer.tv_sec && timercmp(&ctx->now, &ctx->next->pmtCollectionTimer, >=)) {
+		/* We waited too long for all the PMTs to arrive, malformed
+		 * stream or the stream PSI was changed underneath us.
+		 */
+		if (ctx->next->totalPMTsInPAT && ctx->next->parsedPMTs < ctx->next->totalPMTsInPAT) {
+			printf("Stream PMT didn't arrive %d vs %d, aborting\n", ctx->next->totalPMTsInPAT, ctx->next->parsedPMTs);
+			_rom_initialize(ctx, ctx->next, ctx->next->nr);
+			ctx->writePackets = 1;
+		}
 	}
 
-	for (int i = 0; writePackets && i < packetCount; i++) {
+#if 0
+	if (!ctx->writePackets)
+		printf("Not writing packets\n");
+#endif
+
+	for (int i = 0; ctx->writePackets && i < packetCount; i++) {
 
 		uint16_t pid = ltntstools_pid(&pkt[i * 188]);
 		if (pid > 0x1fff) {
@@ -318,7 +430,7 @@ size_t ltntstools_streammodel_write(void *hdl, const unsigned char *pkt, int pac
 			ps->present = 1;
 			ps->pidType = PT_PAT;
 
-			ps->p_dvbpsi = dvbpsi_new(&message, DVBPSI_MSG_DEBUG);
+			ps->p_dvbpsi = dvbpsi_new(&message, DVBPSI_REPORTING);
 
 			if (!dvbpsi_pat_attach(ps->p_dvbpsi, cb_pat, ps))
 			{
@@ -333,6 +445,19 @@ size_t ltntstools_streammodel_write(void *hdl, const unsigned char *pkt, int pac
 
 		ps->packetCount++;
 		ps->lastUpdate = ctx->now;
+
+		if (ctx->restartModel) {
+			ctx->restartModel = 0;
+
+			/* Re-initialize what was current. */
+			_rom_initialize(ctx, ctx->next, ctx->next->nr);
+
+			/* Don't start writing packets into the next model for N time period */
+			struct timeval future = { 0, 500 * 1000 };
+			timeradd(&future, &ctx->now, &ctx->next->allowableWriteTime);
+
+			break;
+		}
 	}
 
 	if (ctx->next->modelComplete) {
@@ -370,19 +495,16 @@ void ltntstools_streammodel_dprintf(void *hdl, int fd)
 	pthread_mutex_unlock(&ctx->rom_mutex);
 }
 
-int ltntstools_streammodel_query_model(void *hdl, struct ltntstools_pat_s **pat)
+static int _streammodel_query_model(struct streammodel_ctx_s *ctx, struct streammodel_rom_s *rom, struct ltntstools_pat_s **pat)
 {
 	int ret = 0;
-printf("%s()\n", __func__);
+//printf("%s() model#%d\n", __func__, rom->nr);
 
-	struct streammodel_ctx_s *ctx = (struct streammodel_ctx_s *)hdl;
+	if (rom->modelComplete) {
 
-	pthread_mutex_lock(&ctx->rom_mutex);
-	if (ctx->current->modelComplete) {
+//		_streammodel_dprintf(ctx, 0, rom);
 
-//		_streammodel_dprintf(ctx, 0, ctx->current);
-
-		struct streammodel_pid_s *ps = &ctx->current->pids[0];
+		struct streammodel_pid_s *ps = &rom->pids[0];
 
 		dvbpsi_pat_t *stream_pat = ps->p_pat;
 		if (stream_pat) {
@@ -394,11 +516,11 @@ printf("%s()\n", __func__);
 				if (newpat->programs[i].program_number == 0)
 					continue; /* Network PID */
 
-				struct streammodel_pid_s *c = _rom_current_find_pid(ctx, newpat->programs[i].program_map_PID);
-				if (c->present == 0 || c->p_pmt == NULL) {
-					fprintf(stderr, "%s() inactive pmt pid? 0x%x, should never happen.\n",
-						__func__, newpat->programs[i].program_map_PID);
-				} else {
+				struct streammodel_pid_s *c = _rom_find_pid(rom, newpat->programs[i].program_map_PID);
+				if (c->present == 0)
+					continue;
+
+				if (c->p_pmt) {
 					ltntstools_pat_add_from_existing(newpat, c->p_pmt);
 				}
 				
@@ -414,8 +536,17 @@ printf("%s()\n", __func__);
 	} else {
 		ret = -1;
 	}
-	pthread_mutex_unlock(&ctx->rom_mutex);
 
 	return ret;
 }
 
+int ltntstools_streammodel_query_model(void *hdl, struct ltntstools_pat_s **pat)
+{
+	struct streammodel_ctx_s *ctx = (struct streammodel_ctx_s *)hdl;
+
+	pthread_mutex_lock(&ctx->rom_mutex);
+	int ret = _streammodel_query_model(ctx, ctx->current, pat);
+	pthread_mutex_unlock(&ctx->rom_mutex);
+
+	return ret;
+}
