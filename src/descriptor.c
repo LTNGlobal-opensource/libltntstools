08c2085aed2ed71769f4dc5afc8bb993687aa263
Steven Toth <steven.toth@ltnglobal.com>
2021-07-15 10:05:10 -0400

    Add additional descriptor parsing support (LTN and CUE)

--- src/descriptor.c
+++ src/descriptor.c
@@ -27,3 +27,43 @@ int ltntstools_descriptor_list_add(struct ltntstools_descriptor_list_s *list,
 	return 0;
 }
 
+int ltntstools_descriptor_list_contains_scte35_cue_registration(struct ltntstools_descriptor_list_s *list)
+{
+	int found = 0;
+
+	for (int i = 0; i < list->count; i++) {
+		struct ltntstools_descriptor_entry_s *d = &list->array[i];
+
+		if (d->tag == 0x05 && d->len == 0x04) {
+			if (d->data[0] == 'C' && d->data[1] == 'U' && d->data[2] == 'E' && d->data[3] == 'I') {
+				found = 1;
+				break;
+			}
+		}
+	}
+
+	return found;
+}
+
+int ltntstools_descriptor_list_contains_ltn_encoder_sw_version(struct ltntstools_descriptor_list_s *list,
+	unsigned int *major, unsigned int *minor, unsigned int *patch)
+{
+	int found = 0;
+
+	for (int i = 0; i < list->count; i++) {
+		struct ltntstools_descriptor_entry_s *d = &list->array[i];
+
+		/* Look for tag a2 length 4 and embedded tag 1 (sw version) */
+
+		if (d->tag == 0xa2 && d->len == 0x04 && d->data[0] == 0x01) {
+			*major = d->data[1];
+			*minor = d->data[2];
+			*patch = d->data[3];
+			found = 1;
+			break;
+		}
+	}
+
+	return found;
+}
+
