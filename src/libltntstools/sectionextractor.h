538fe40b576acbc5e9c80293cfee0d2cef8a7197
Steven Toth <steven.toth@ltnglobal.com>
2022-03-25 10:44:46 -0400

    sectionextractor: doxygen

--- src/libltntstools/sectionextractor.h
+++ src/libltntstools/sectionextractor.h
@@ -1,15 +1,14 @@
 #ifndef _SECTIONEXTRACTOR_H
 #define _SECTIONEXTRACTOR_H
 
-/* A minimalistic implemenation of a section extractor, it has significant
- * limitations, namely:
- * 1. Only sections less than 180 bytes are supported.
- * 2. sections MUST be select contained with a single TS packet.
- * 3. TS packets must have the packet marker set, which they should anyway, for the start of a new packet.
- * Good enough for some basic lab testing with SCTE35.
+/**
+ * @file        sectionextractor.h
+ * @author      Steven Toth <steven.toth@ltnglobal.com>
+ * @copyright   Copyright (c) 2020-2022 LTN Global,Inc. All Rights Reserved.
+ * @brief       Framework to demux/extract MPEG-TS tables and sections
+ *              Heavily leaveraged from bits of libiso13818.
+ * 
  */
-
-/* Heavily leaveraged from bits of libiso13818 */
 
 #include <time.h>
 #include <inttypes.h>
@@ -18,10 +17,43 @@
 extern "C" {
 #endif
 
-int     ltntstools_sectionextractor_alloc(void **hdl, uint16_t pid, uint8_t tableID);
-ssize_t ltntstools_sectionextractor_write(void *hdl, const uint8_t *pkt, size_t packetCount, int *complete, int *crcValid);
-void    ltntstools_sectionextractor_free(void *hdl);
-int     ltntstools_sectionextractor_query(void *hdl, uint8_t *dst, int lengthBytes);
+/**
+ * @brief       Allocate a framework context capable of table/section demux and extraction
+ * @param[in]   void **hdl - Handle / context for further use.
+ * @param[in]   uint16_t pid - MPEG TS transport PID to be de-muxed
+ * @param[in]   uint8_t tableId - Eg. 0xFC (SCTE35)
+ * @return      0 on success, else < 0.
+ */
+int ltntstools_sectionextractor_alloc(void **hdl, uint16_t pid, uint8_t tableID);
+
+/**
+ * @brief       Write an entire MPTS into the framework, pid filtering and demux the stream.
+ *              Once an entire PES has been parsed, the caller may query the extracted
+ *              table/section once complete is 1, and optionally choose to ignore or make
+ *              note of the fact the table was corrupt via crcValid.
+ * @param[in]   void *hdl - Handle / context.
+ * @param[in]   const uint8_t *pkts - one or more aligned transport packets
+ * @param[in]   int packetCount - number of packets
+ * @param[out]  int *complete - output flag boolean. zero when parsing, one when caller should call ltntstools_sectionextractor_query()
+ * @param[out]  int *crcValid - boolean
+ * @return      number of packets processed
+ */
+ssize_t ltntstools_sectionextractor_write(void *hdl, const uint8_t *pkts, size_t packetCount, int *complete, int *crcValid);
+
+/**
+ * @brief       Free a previously allocate context.
+ * @param[in]   void *hdl - Handle / context.
+ */
+void ltntstools_sectionextractor_free(void *hdl);
+
+/**
+ * @brief       Allocate a framework context capable of table/section demux and extraction
+ * @param[in]   void **hdl - Handle / context for further use.
+ * @param[out]  uint8_t *dst - user allocated buffer to contain the raw table/section
+ * @param[in]   int lengthBytes - length of the destination buffer.
+ * @return      number of bytes written
+ */
+int ltntstools_sectionextractor_query(void *hdl, uint8_t *dst, int lengthBytes);
 
 #ifdef __cplusplus
 };
