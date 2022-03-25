442ad01b6c3953d3b10b41f87aa590c6d70c7bee
Steven Toth <steven.toth@ltnglobal.com>
2022-03-25 10:49:55 -0400

    ts_packetizer: doxygen

--- src/libltntstools/ts_packetizer.h
+++ src/libltntstools/ts_packetizer.h
@@ -22,13 +22,30 @@
 #ifndef TS_PACKETIZER_H
 #define TS_PACKETIZER_H
 
+/**
+ * @file        ts_packetizer.h
+ * @author      Steven Toth <steven.toth@ltnglobal.com>
+ * @copyright   Copyright (c) 2016 Kernel Labs Inc. All Rights Reserved
+ * @brief       Convert a buffer of bytes into transport packets (minimal approach)
+ */
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Zero on success else < 0 on error. */
+/**
+ * @brief       Convert input buffer into multiple output transport packets.
+ *              The caller owns the lifepan of the output pkts array, be sure to free it.
+ * @param[in]   const uint8_t *buf - buffer of bytes to packetize
+ * @param[in]   unsigned int byteCount - length of buffer in bytes
+ * @param[out]  uint8_t **pkts - output array
+ * @param[out]  uint32_t *packetCount - number of elements in the output array
+ * @param[in]   int packetSize - Typically 188
+ * @param[in]   uint8_t *cc - user allocate storage where the CC counter will be maintained.
+ * @param[in]   uint16_t pid - transport packet identifier for the output packets.
+ * @return      0 on success, else < 0.
+ */
 int ltntstools_ts_packetizer(const uint8_t *buf, unsigned int byteCount,
 	uint8_t **pkts, uint32_t *packetCount, int packetSize, uint8_t *cc, uint16_t pid);
 
