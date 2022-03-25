66abbfa4e3995e8846c438f448066315c0c98135
Steven Toth <steven.toth@ltnglobal.com>
2022-03-25 09:50:35 -0400

    clocks: add doxygen comments

--- src/libltntstools/clocks.h
+++ src/libltntstools/clocks.h
@@ -8,6 +8,21 @@
 extern "C" {
 #endif
 
+/*
+ * Walltime: time on the running host, measured in uS.
+ * Timebase: A clock runnig at a rate of 27MHz, or 90Khz, or any other rate, for example.
+ * Ticks: a unit of measure, generally related to the timebase
+ * 
+ * Walltime moves naturally on its own, it advanced.
+ * The clock context and its timebase does not, it only moves when the caller
+ * passes in new tick values, as additional increments or as an absolute value.
+ * 
+ */
+
+/**
+ * @brief API context
+ * 
+ */
 struct ltntstools_clock_s
 {
 	int establishedTimebase;
@@ -27,45 +42,94 @@ struct ltntstools_clock_s
 	int64_t clockWrapValue; /* Upper limited so we can compute wrapping clocks. */
 };
 
-/* First call any user should make, it to initialize the context. */
+/**
+ * @brief       First call any user should make, it to initialize the context.
+ *              Once initialized, the clock context can be used routinely with the other APIs.
+ * @param[in]   struct ltntstools_clock_s *clk - user allocated storage for the clock context
+ */
 void ltntstools_clock_initialize(struct ltntstools_clock_s *clk);
 
-/* Erase the context.
- * Establish a timebase of N ticks_per_second.
- * For example:     90,000 FOR PTS/DTS
- *              27,000,000 FOR PCR
+/**
+ * @brief       Establish a timebase of N ticks_per_second, for this clock context.
+ *              For example:     90,000 FOR PTS/DTS
+ *                           27,000,000 FOR PCR
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @param[in]   int64_t ticks_per_second - Eg. 27,000,000
  */
 void ltntstools_clock_establish_timebase(struct ltntstools_clock_s *clk, int64_t ticks_per_second);
+
+/**
+ * @brief       Determine if the timebase has been established, via an earlier call to ltntstools_clock_establish_timebase()
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @return      Boolean. 1 on success else 0.
+ */
 int  ltntstools_clock_is_established_timebase(struct ltntstools_clock_s *clk);
 
-/* One the timebase bas been established (ltntstools_clock_reset()),
- * syncronize 'ticks' (and absolute measurement of time int he establish timebase,
- * to walltime (measured in us).
- * 
- * This measns we'll be able to measure drift, in the future.
+/**
+ * @brief       Once the timebase bas been established (ltntstools_clock_reset()),
+ *              syncronize 'ticks' (and absolute measurement of time int he establish timebase,
+ *              to walltime (measured in us).
+ *              This measns we'll be able to measure drift, in the future.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @param[in]   int64_t ticks - current measurement for the clock context (that will be aligned to walltime).
  */
 void ltntstools_clock_establish_wallclock(struct ltntstools_clock_s *clk, int64_t ticks);
+
+/**
+ * @brief       Determine if the wallclock has been established, via an earlier call to ltntstools_clock_establish_wallclock()
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @return      Boolean. 1 on success else 0.
+ */
 int  ltntstools_clock_is_established_wallclock(struct ltntstools_clock_s *clk);
 
-/* Set the current time (in timebase) to 'ticks'.
- * For example, after you've extracted a PTS from the PES, or a PCR clock from a frame,
- * you should call this method to syncronize the timebase to walltime.
- * In all cases, 'ticks' should be a positive value.
+/**
+ * @brief       Set the current timebase clock to an absolute value of 'ticks'.
+ *              For example, after you've extracted a PTS from the PES, or a PCR clock from a frame,
+ *              you should call this method to syncronize the timebase to walltime.
+ *              In all cases, 'ticks' should be a positive value.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @param[in]   int64_t ticks - absolute value
  */
 void ltntstools_clock_set_ticks(struct ltntstools_clock_s *clk, int64_t ticks);
 
+/**
+ * @brief       Get the current timebase clock to an absolute value of 'ticks'.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @return      int64_t ticks - current measurement from clock context.
+ */
 int64_t ltntstools_clock_get_ticks(struct ltntstools_clock_s *clk);
 
-/* While normally _set_ticks is used to syncronize the timebase, you may wish
- * to closely control a clock and add N ticks to the timebase.
- * You may add a positive or negative value to bend the timebase value, if desired.
+/**
+ * @brief       Add N ticks to the existing clock context, positive or negative values are supported.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @param[in]   int64_t ticks - relative value
  */
 void ltntstools_clock_add_ticks(struct ltntstools_clock_s *clk, int64_t ticks);
 
-/* Query the */
+/**
+ * @brief       Query the drift between walltime and the timebase context, expressed in uS.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @return      int64_t - ticks
+ */
 int64_t ltntstools_clock_get_drift_us(struct ltntstools_clock_s *clk);
+
+/**
+ * @brief       Query the drift between walltime and the timebase context, expressed in mS.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @return      int64_t - ticks
+ */
 int64_t ltntstools_clock_get_drift_ms(struct ltntstools_clock_s *clk);
 
+/**
+ * @brief       Compute the delta (always expressed as positive) between two timebase values.
+ *              For example, computing the difference between two tick values in this timebase,
+ *              regardless of whether the clock will wrap or not.
+ *              Supported for 27MHz and 90 kHz clocks.
+ * @param[in]   struct ltntstools_clock_s *clk - context
+ * @param[in]   int64_t ticks - timeA
+ * @param[in]   int64_t ticks - timeB
+ * @return      int64_t - ticks
+ */
 int64_t ltntstools_clock_compute_delta(struct ltntstools_clock_s *clk, int64_t ticksnow, int64_t ticksthen);
 
 #ifdef __cplusplus
