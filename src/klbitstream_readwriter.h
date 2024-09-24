e127b6ad69b514422e36a46500b9f4d569d12207
Chris Kennedy <chris.kennedy@ltnglobal.com>
2024-09-24 15:43:35 -0400

    avoid infinite loop
    this seems to cause it to never recover when we return 0

--- src/klbitstream_readwriter.h
+++ src/klbitstream_readwriter.h
@@ -257,7 +257,7 @@ static uint64_t klbs_read_byte_aligned(struct klbs_context_s *ctx)
 	if (ctx->buflen_used >= ctx->buflen) {
 		fprintf(stderr, "KLBITSTREAM WARNING: %s ctx->buflen_used %d >= ctx->buflen %d\n", __func__, ctx->buflen_used, ctx->buflen);
 		ctx->error = 1;
-		return 0;
+		//return 0;
 	}
 	return *(ctx->buf + ctx->buflen_used++);
 }
