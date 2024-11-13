fda6f7f76b4bfb78f0f128b146256033ab15c04a
Steven Toth <steven.toth@ltnglobal.com>
2024-11-13 10:02:21 -0500

    enhance PAT object with helper functions for PMT manipuldation and TS packet creation

--- src/libltntstools/pat.h
+++ src/libltntstools/pat.h
@@ -55,6 +55,25 @@ struct ltntstools_pmt_s
 int ltntstools_pmt_compare(struct ltntstools_pmt_s *a, struct ltntstools_pmt_s *b);
 
 /**
+ * @brief       For a given PMT object, search of the ES stream using PID X and remove it.
+ * @param[in]   struct ltntstools_pmt_s *pmt - object
+ * @param[in]   uint16_t pid - final TS pid for output packet.
+ * @return      0 - Success or, < 0 on error.
+ */
+int ltntstools_pmt_remove_es_for_pid(struct ltntstools_pmt_s *pmt, uint16_t pid);
+
+/**
+ * @brief       Convert a pmt object into a fully formed transport packet (EXACTLY ONE packet, not longer). TODO. Support longer PMTs.
+ * @param[in]   struct ltntstools_pmt_s *pmt - object
+ * @param[in]   uint16_t pid - final TS pid for output packet.
+ * @param[in]   uint8_t cc - desired initial continuity counter value
+ * @param[in]   uint8_t *packet - Buffer where packet will be created
+ * @param[in]   int packetLengthBytes - Buffer length, must be exactly 188 bytes.
+ * @return      0 - Success or, < 0 on error.
+ */
+int ltntstools_pmt_create_packet_ts(struct ltntstools_pmt_s *pmt, uint16_t pid, uint8_t cc, uint8_t *packet, int packetLengthBytes);
+
+/**
  * @brief       PAT table entry
  */
 struct ltntstools_pat_program_s
@@ -92,6 +111,13 @@ struct ltntstools_pat_s *ltntstools_pat_alloc();
 void ltntstools_pat_free(struct ltntstools_pat_s *pat);
 void ltntstools_pat_dprintf(struct ltntstools_pat_s *pat, int fd);
 
+/**
+ * @brief       Duplicate the entire PAT object into a new memory allocation.
+ * @param[in]   struct ltntstools_pat_s *pat - object
+ * @return      New allocation on Success, else NULL.
+ */
+struct ltntstools_pat_s *ltntstools_pat_clone(struct ltntstools_pat_s *pat);
+
 /* Helper functions from libdvbpsi to the internal model. */
 typedef struct dvbpsi_pat_s dvbpsi_pat_t;
 struct ltntstools_pat_s * ltntstools_pat_alloc_from_existing(dvbpsi_pat_t *pat);
@@ -128,9 +154,19 @@ int ltntstools_pat_enum_services_smpte2038(struct ltntstools_pat_s *pat, int *e,
  */
 int ltntstools_pat_enum_services_video(struct ltntstools_pat_s *pat, int *e, struct ltntstools_pmt_s **pmt);
 
+/**
+ * @brief       Enumerate all services in the PAT object, return the associated PMT.
+ * @param[in]   struct ltntstools_pat_s *pat - object
+ * @param[in]   int *e - used internally to enumerate objects. Pass 0 value int on first call then don't modify afterwards
+ * @param[out]  struct ltntstools_pmt_s **pmt - ptr to the pmt object containing in the PAT
+ * @return      0 - Success, PMT and PID contain details. < 0, no nore SCTE35 sevices, or error.
+ */
+int ltntstools_pat_enum_services(struct ltntstools_pat_s *pat, int *e, uint16_t pid, struct ltntstools_pmt_s **pmt);
+
 #ifdef __cplusplus
 };
 #endif
 
-#endif /* TR101290_PAT_H */
 int ltntstools_pmt_query_video_pid(struct ltntstools_pmt_s *pmt, uint16_t *pid, uint8_t *estype);
+
+#endif /* TR101290_PAT_H */
