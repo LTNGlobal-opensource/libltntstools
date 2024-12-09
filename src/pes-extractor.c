da001a5c3e9d5d755a0dffcc2b14d65b5ac4ae01
Chris Kennedy <c@groovy.org>
2024-12-09 07:17:29 -0800

    Revert "pes-extractor: Handle buffer overrun better"
    This reverts commit 3c18aa253dc7d8a71ce01678e26829320918427c.

--- src/pes-extractor.c
+++ src/pes-extractor.c
@@ -207,7 +207,6 @@ static int _processRing(struct pes_extractor_s *ctx)
 	if (rlen < 16)
 		return -1;
 	int overrun = 0;
-	int offset = -1;
 
 #if LOCAL_DEBUG
 	printf("%s() ring size %d\n", __func__, rb_used(ctx->rb));
@@ -225,7 +224,7 @@ static int _processRing(struct pes_extractor_s *ctx)
 			 * meaningless, becasue the buffer is expected to contain and ENTIRE PES followed by the header from
 			 * a subsequence PES.
 			 */
-			offset = searchReverse(buf, rlen, ctx->streamId);
+			int offset = searchReverse(buf, rlen, ctx->streamId);
 			if (offset < 16) {
 				/* We'll come back again in the future */
 				free(buf);
@@ -259,15 +258,6 @@ static int _processRing(struct pes_extractor_s *ctx)
 #if KLBITSTREAM_RETURN_ON_OVERRUN
 				ltn_pes_packet_free(pes);
 				free(buf);
-
-				if (offset > 0 && offset <= rb_used(ctx->rb))
-				{
-					rb_discard(ctx->rb, offset);
-				}
-				else
-				{
-					rb_empty(ctx->rb);
-				}
 				return -2;
 #else
 				overrun = 1;
@@ -329,9 +319,10 @@ static int _processRing(struct pes_extractor_s *ctx)
 		free(buf);
 	}
 
-	if (!overrun && offset > 0)
-	{
-		rb_discard(ctx->rb, offset);
+	uint8_t tbuf[16];
+	size_t l = rb_read(ctx->rb, (char *)&tbuf[0], sizeof(tbuf));
+	if (l == 16) {
+		//ltntstools_hexdump(buf, sizeof(tbuf), 16);
 	}
 
 #if LOCAL_DEBUG
